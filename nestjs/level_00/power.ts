7122b867bf00fadda090ac387981694a6c039504
fikkar <fikkar@gitlab.lab>
2024-01-30 16:30:25 +0700

    power function

---
+++ nestjs/level_00/power.ts
@@ -0,0 +1,18 @@
+/*
+2. Buatlah fungsi bernama power dengan ketentuan berikut:
+ - Menerima dua buah argumen number, a dan b.
+ - Mengembalikan nilai dari hasil perkalian a * a sebanyak b (fungsi kuadrat).
+
+ contoh:
+ power(7, 3) // 343
+ power(3, 3) // 27
+ power(4, 0.5) // 2
+*/
+
+function power(a, b) {
+    return a ** b
+}
+
+console.log(power(7, 3));
+console.log(power(3, 3));
+console.log(power(4, 0.5));
